edf46cc0f0798961cd36deb783a99d4a03511788
yangshuai <yangshuai@p1.com>
2021-05-08 15:40:12 +0800

    feat: test

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,13 @@
 /** @format */
 
 module.exports = {
-  printWidth: 120,
+  printWidth: 100,
   semi: true,
   singleQuote: true,
   trailingComma: 'all',
   bracketSpacing: false,
-  jsxBracketSameLine: false,
-  arrowParens: 'avoid',
+  jsxBracketSameLine: true,
+  // arrowParens: 'avoid',
   tabWidth: 2,
   useTabs: false,
 };
