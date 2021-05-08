485d76b61999f4d365c83c543e22f853e12e4c4c
yangshuai <yangshuai@p1.com>
2021-05-08 15:47:14 +0800

    feat: test

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,10 +1,12 @@
-import React, {useReducer} from 'react';
-import {ContextState, reducer, initState} from './State';
+import React, { useReducer } from 'react';
+import { ContextState, reducer, initState } from './State';
 
 const ContextProvider: React.FC = (props) => {
-  console.log(111);
+  console.log(1111);
   const [state, dispatch] = useReducer(reducer, initState);
-  return <ContextState.Provider value={{state, dispatch}}>{props.children}</ContextState.Provider>;
+  return (
+    <ContextState.Provider value={{ state, dispatch }}>{props.children}</ContextState.Provider>
+  );
 };
 
 export default ContextProvider;
